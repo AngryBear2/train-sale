768136dc32c1e06ce6d24e986ca9d83ae492dc6d
telek <2495443090@qq.com>
2022-07-08 17:19:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+const app= {
+  name: 'App',
+  data() {
+    return {
+      zhCn
+    }
+  }
+}
+</script>
+
+<template>
+<el-config-provider :locale="zhCn">
+     <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-date-picker/> 
+  </el-config-provider>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
